141f2d546be27bd244500a0df5f0caffa3cc4006
HP <HP@DESKTOP-6TU6KJT>
2021-01-25 00:33:24 +0300

    commit rainbow

---
+++ rainbowSenla/src/rainbowSenla/Main.java
@@ -0,0 +1,23 @@
+package rainbowSenla;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Rainbow rainbow = new Rainbow();
+       rainbow.rainBow(1);
+       rainbow.rainBow(2);
+       rainbow.rainBow(3);
+       rainbow.rainBow(4);
+       rainbow.rainBow(5);
+       rainbow.rainBow(6);
+       rainbow.rainBow(7);
+       rainbow.rainBow(12);
+       rainbow.rainBow(21);
+       rainbow.rainBow(23);
+       rainbow.rainBow(32);
+       
+}
+	}
+
+
---
+++ rainbowSenla/src/rainbowSenla/Rainbow.java
@@ -0,0 +1,74 @@
+package rainbowSenla;
+
+
+
+
+public class Rainbow {
+
+	  private static final int RED = 1;
+	  private static final int ORANGE = 2;
+	  private static final int YELLOW = 3;
+	  private static final int GREEN = 4;
+	  private static final int LIGHTBLUE = 5;
+	  private static final int BLUE = 6;
+	  private static final int PURPLE = 7;
+	  
+	  
+	  
+	  public void rainBow(int number) {
+      
+      switch(number) {
+          case RED:
+        	  System.out.println("red");
+        	  break;
+          case ORANGE:
+        	  System.out.println("orange");
+        	  break;
+          case YELLOW:
+        	  System.out.println("yellow");
+        	  break;
+          case GREEN:
+        	  System.out.println("green");
+        	  break;
+          case LIGHTBLUE:
+        	  System.out.println("lightBlue");
+        	  break;
+          case BLUE:
+        	  System.out.println("blue");
+        	  break;
+          case PURPLE:
+        	  System.out.println("purple");
+        	  break;
+          
+                  		  
+		  
+		  case 12:
+			  System.out.println("red-orange");
+			  break;
+		  case 21:
+			  System.out.println("red-orange");
+			  break;
+		  case 23:
+			  System.out.println("yellow-orange");
+			  break;
+		  case 32:
+			  System.out.println("yellow-orange");
+			  break;
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  }
+		
+		
+		  
+		  
+		  
+		  
+	  }
+	  
+	  
+}
